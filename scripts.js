65b086451cb7b29e3e4ba2069ac30e26824529be
Alan42121 <alanantonio78@gmail.com>
2025-07-14 18:53:12 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,5 @@
-function toggleEstado(elemento) {
-  if (elemento.classList.contains("pendiente")) {
-    elemento.classList.remove("pendiente");
-    elemento.classList.add("en-curso");
-  } else if (elemento.classList.contains("en-curso")) {
-    elemento.classList.remove("en-curso");
-    elemento.classList.add("aprobado");
-  } else if (elemento.classList.contains("aprobado")) {
-    elemento.classList.remove("aprobado");
-    elemento.classList.add("pendiente");
-  } else {
-    elemento.classList.add("pendiente");
-  }
-}
+document.querySelectorAll('.ramo').forEach(ramo => {
+  ramo.addEventListener('click', () => {
+    ramo.classList.toggle('aprobado');
+  });
+});
